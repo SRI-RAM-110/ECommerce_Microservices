d9de79c930e6b8110f17255a1e18cf36dbc04b02
Kiran-2526 <callmesiddhik@gmail.com>
2026-06-08 19:03:38 +0530

    login page is updated

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "../styles/login.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='body'>
+      <div className='login-container'>
+        <img src="https://images.pexels.com/photos/7501135/pexels-photo-7501135.jpeg" alt="not-found"/>
+      </div>
+    </div>
   )
 }
 
