beeb7b53313579f60e1f69eb3e15b7af4a859460
Kiran-2526 <callmesiddhik@gmail.com>
2026-06-08 19:30:04 +0530

    login-updated

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -5,7 +5,12 @@ const Login = () => {
   return (
     <div className='body'>
       <div className='login-container'>
-        <img src="https://images.pexels.com/photos/7501135/pexels-photo-7501135.jpeg" alt="not-found"/>
+        <div className='image'>
+          <img src="https://images.pexels.com/photos/7501135/pexels-photo-7501135.jpeg" alt="not-found"/>
+        </div>
+        <div className='login-credentials'>
+          <h1> Welcome Back</h1>
+        </div>
       </div>
     </div>
   )
